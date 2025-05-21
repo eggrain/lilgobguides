cca422a9398b0764eee1e459bf1385df9c3637da
Kyle Rego <regoky@outlook.com>
2025-05-21 15:51:03 -0700

    Add delete post

--- Pages/Posts/ShowPost.cshtml.cs
+++ Pages/Posts/ShowPost.cshtml.cs
@@ -10,18 +10,33 @@ public class ShowPostModel(AppDbContext db) : PageModel
 {
     private readonly AppDbContext _db = db;
 
-    public Post? Post { get; set;  }
+    public Post Post { get; set; } = new();
 
     public async Task<IActionResult> OnGetAsync(int? id)
     {
         if (id == null) return NotFound();
 
-        Post = await _db.Posts.AsNoTracking()
+        Post? post = await _db.Posts.AsNoTracking()
             .FirstOrDefaultAsync(p => p.Id == id);
 
-        if (Post == null) return NotFound();
+        if (post == null) return NotFound();
 
+        Post = post;
         return Page();
+    }
+
+    public async Task<IActionResult> OnPostDeleteAsync(int? id)
+    {
+        if (id == null) return NotFound();
+
+        Post? post = await _db.Posts.AsNoTracking()
+            .FirstOrDefaultAsync(p => p.Id == id);
+        if (post == null) return NotFound();
+
+        _db.Posts.Remove(post);
+        await _db.SaveChangesAsync();
+
+        return RedirectToPage("/Index");
     }
 }
 
