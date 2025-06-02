56ae160334ab75c8c50fa85f69aa0f73f62446a1
Kyle Rego <regoky@outlook.com>
2025-06-02 13:17:02 -0700

    Cleanup

--- Pages/Posts/EditPost.cshtml.cs
+++ Pages/Posts/EditPost.cshtml.cs
@@ -11,22 +11,14 @@ public class EditPostModel(AppDbContext db) : PageModel
     private readonly AppDbContext _db = db;
 
     [BindProperty]
-    public Post Post { get; set; } = new();
-
-    private async Task<Post?> LoadPost(string id, bool asNoTracking)
-    {
-        var q1 = _db.Posts.Include(p => p.Categorization);
-
-        var q2 = (asNoTracking == false) ? q1 : q1.AsNoTracking();
-
-        return await q2.FirstOrDefaultAsync(p => p.Id == id);
-    }
+    public Post Post { get; set; } = null!;
 
     public async Task<IActionResult> OnGetAsync(string? id)
     {
         if (id == null) return NotFound();
 
-        Post? post = await LoadPost(id, true);
+        Post? post = await _db.Posts.Where(p => p.Id == id)
+                    .AsNoTracking().FirstOrDefaultAsync();
 
         if (post == null) return NotFound();
         Post = post;
@@ -35,31 +27,25 @@ public class EditPostModel(AppDbContext db) : PageModel
 
     public async Task<IActionResult> OnPostAsync(string id)
     {
-        Console.WriteLine($"Prebind: Post.Categorization == null? {Post.Categorization == null}");
-        foreach (var kv in ModelState
-           .Where(kv => kv.Value.Errors.Count > 0))
-        {
-            Console.WriteLine($"{kv.Key}: {string.Join(", ", kv.Value.Errors.Select(e => e.ErrorMessage))}");
-        }
-
         if (!ModelState.IsValid)
         {
-            Console.WriteLine("It was not valid -");
+            Console.WriteLine("Model validation failed");
             return Page();
         }
 
-        Post? existing = await LoadPost(id, false);
+        Post? post = await _db.Posts.Where(p => p.Id == id)
+                            .FirstOrDefaultAsync();
 
-        if (existing == null) return NotFound();
+        if (post == null) return NotFound();
 
-        existing.Categorization ??= new() { PostId = Post.Id };
+        post.Categorization ??= new() { PostId = Post.Id };
 
-        existing.Title = Post.Title;
-        existing.Content = Post.Content;
-        existing.Categorization.Skilling = Post.Categorization.Skilling;
-        existing.Categorization.Minigame = Post.Categorization.Minigame;
-        existing.Categorization.Item = Post.Categorization.Item;
-        existing.Categorization.Boss = Post.Categorization.Boss;
+        post.Title = Post.Title;
+        post.Content = Post.Content;
+        post.Categorization.Skilling = Post.Categorization.Skilling;
+        post.Categorization.Minigame = Post.Categorization.Minigame;
+        post.Categorization.Item = Post.Categorization.Item;
+        post.Categorization.Boss = Post.Categorization.Boss;
 
         await _db.SaveChangesAsync();
 
